653123804cdda70897d4daa3fc1d5281d365a561
lê phú hảo <lephuhao24@gmail.com>
2019-10-27 11:49:31 +0700

    first upload

--- server/index.js
+++ server/index.js
@@ -11,6 +11,16 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+if(process.abort.env.NODE_ENV === 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //handle spa
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html')
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
